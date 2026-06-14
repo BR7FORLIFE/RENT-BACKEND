012eacca6fcf0b0ec48585b1bf06fb7cb433f998
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-06-14 09:05:31 -0500

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ rent-auth-module/src/main/java/com/files/rent_auth_module/RentAuthModuleApplication.java
@@ -0,0 +1,13 @@
+package com.files.rent_auth_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentAuthModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentAuthModuleApplication.class, args);
+	}
+
+}
---
+++ rent-auth-module/src/test/java/com/files/rent_auth_module/RentAuthModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.files.rent_auth_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentAuthModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
