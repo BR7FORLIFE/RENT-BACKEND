c8b99509ad82b5c51e517ab59423e8dadb66e3c8
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-06-22 12:21:12 -0500

    Refactor EmailVerificationUseCaseImp to enhance email verification token handling and improve code readability

--- rent-auth-module/src/main/java/com/files/rent_auth_module/application/emailVerification/orchestator/EmailVerificationUseCaseImp.java
+++ rent-auth-module/src/main/java/com/files/rent_auth_module/application/emailVerification/orchestator/EmailVerificationUseCaseImp.java
@@ -29,99 +29,101 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class EmailVerificationUseCaseImp implements EmailVerificationUseCase {
-    private final ResendPort resendPort;
-    private final EmailVerificationPort emailVerificationPort;
-    private final GenerateCodeService generateCodeService;
-    private final AuthRepositoryPort authRepositoryPort;
+        private final ResendPort resendPort;
+        private final EmailVerificationPort emailVerificationPort;
+        private final GenerateCodeService generateCodeService;
+        private final AuthRepositoryPort authRepositoryPort;
 
-    @Value("${resend.expiration-time}")
-    private Long expirationTimeEmail;
+        @Value("${resend.expiration-time}")
+        private Long expirationTimeEmail;
 
-    @Value("${resend.expiration-forward-time}")
-    private Long forwardTime;
+        @Value("${resend.expiration-forward-time}")
+        private Long forwardTime;
 
-    public EmailVerificationUseCaseImp(ResendPort resendPort,
-            EmailVerificationPort emailVerificationPort,
-            GenerateCodeService generateCodeService,
-            AuthRepositoryPort authRepositoryPort) {
-        this.resendPort = resendPort;
-        this.emailVerificationPort = emailVerificationPort;
-        this.generateCodeService = generateCodeService;
-        this.authRepositoryPort = authRepositoryPort;
-    }
+        public EmailVerificationUseCaseImp(ResendPort resendPort,
+                        EmailVerificationPort emailVerificationPort,
+                        GenerateCodeService generateCodeService,
+                        AuthRepositoryPort authRepositoryPort) {
+                this.resendPort = resendPort;
+                this.emailVerificationPort = emailVerificationPort;
+                this.generateCodeService = generateCodeService;
+                this.authRepositoryPort = authRepositoryPort;
+        }
 
-    @Transactional
-    @Override
-    public Mono<Void> sendEmailVerificationToken(UUID userId, String email) {
-        String code = generateCodeService.sha256(generateCodeService.randomBase64(128));
-        Instant expirationTime = Instant.now().plusSeconds(expirationTimeEmail);
+        @Transactional
+        @Override
+        public Mono<Void> sendEmailVerificationToken(UUID userId, String email) {
+                String code = generateCodeService.sha256(generateCodeService.randomBase64(128));
+                Instant expirationTime = Instant.now().plusSeconds(expirationTimeEmail);
 
-        EmailVerificationModel emailModel = EmailVerificationModel.createDraft(userId, code, expirationTime);
+                EmailVerificationModel emailModel = EmailVerificationModel.createDraft(userId, code, expirationTime);
 
-        return Mono.when(emailVerificationPort.save(emailModel), resendPort.sendEmail(email, code)).then();
-    }
+                return Mono.when(emailVerificationPort.save(emailModel), resendPort.sendEmail(email, code)).then();
+        }
 
-    @Override
-    public Mono<EmailForwardCommandResult> forwardToken(EmailForwardCommand cmd) {
-        return authRepositoryPort.findById(cmd.userId())
-                .switchIfEmpty(Mono.error(AuthExceptions.userNotFound()))
-                .zipWhen(user -> emailVerificationPort
-                        .findFirstByUserIdOrderByCreatedAtDesc(user.getId())
-                        .switchIfEmpty(Mono.error(new NotEmailVerificationException())))
-                .flatMap(tuple -> {
-                    UserModel user = tuple.getT1();
-                    EmailVerificationModel emailVerificationModel = tuple.getT2();
+        @Override
+        public Mono<EmailForwardCommandResult> forwardToken(EmailForwardCommand cmd) {
+                return authRepositoryPort.findById(cmd.userId())
+                                .switchIfEmpty(Mono.error(AuthExceptions.userNotFound()))
+                                .zipWhen(user -> emailVerificationPort
+                                                .findFirstByUserIdOrderByCreatedAtDesc(user.getId())
+                                                .switchIfEmpty(Mono.error(new NotEmailVerificationException())))
+                                .flatMap(tuple -> {
+                                        UserModel user = tuple.getT1();
+                                        EmailVerificationModel emailVerificationModel = tuple.getT2();
 
-                    if (emailVerificationModel.getCreateAt()
-                            .plusSeconds(forwardTime)
-                            .isAfter(Instant.now())) {
-                        return Mono.error(new EmailVerificationCooldownException());
-                    }
+                                        if (emailVerificationModel.getCreateAt()
+                                                        .plusSeconds(forwardTime)
+                                                        .isAfter(Instant.now())) {
+                                                return Mono.error(new EmailVerificationCooldownException());
+                                        }
 
-                    return emailVerificationPort
-                            .revockedAllEmailVerification(user.getId())
-                            .then(sendEmailVerificationToken(
-                                    user.getId(),
-                                    user.getEmail().trim()))
-                            .thenReturn(new EmailForwardCommandResult("email forward!"));
-                });
-    }
+                                        return emailVerificationPort
+                                                        .revockedAllEmailVerification(user.getId())
+                                                        .then(sendEmailVerificationToken(
+                                                                        user.getId(),
+                                                                        user.getEmail().trim()))
+                                                        .thenReturn(new EmailForwardCommandResult("email reenviado!"));
+                                });
+        }
 
-    @Override
-    public Mono<EmailVerificationCommandResult> verificationToken(EmailVerificationCommand cmd) {
-        return emailVerificationPort.findByToken(cmd.token())
-                .switchIfEmpty(Mono.error(new NotEmailVerificationException()))
-                .doOnNext(EmailVerificationService::validateEmailVerification)
-                .zipWhen(token -> authRepositoryPort.findById(token.getUserId())
-                        .switchIfEmpty(Mono.error(AuthExceptions.userNotFound())))
-                .flatMap(tuple -> {
-                    EmailVerificationModel token = tuple.getT1();
-                    UserModel user = tuple.getT2();
+        @Override
+        public Mono<EmailVerificationCommandResult> verificationToken(EmailVerificationCommand cmd) {
+                return emailVerificationPort.findByToken(cmd.token())
+                                .switchIfEmpty(Mono.error(new NotEmailVerificationException()))
+                                .doOnNext(EmailVerificationService::validateEmailVerification)
+                                .zipWhen(token -> authRepositoryPort.findById(token.getUserId())
+                                                .switchIfEmpty(Mono.error(AuthExceptions.userNotFound())))
+                                .flatMap(tuple -> {
+                                        EmailVerificationModel token = tuple.getT1();
+                                        UserModel user = tuple.getT2();
 
-                    if (token.getExpiredAt().isBefore(Instant.now())) {
-                        EmailVerificationModel revockedCurrentEmail = EmailVerificationModel
-                                .changeStatusEmail(token, StatusEmail.REVOKED);
+                                        if (token.getExpiredAt().isBefore(Instant.now())) {
+                                                EmailVerificationModel revockedCurrentEmail = EmailVerificationModel
+                                                                .changeStatusEmail(token, StatusEmail.REVOKED);
 
-                        emailVerificationPort.save(revockedCurrentEmail);
+                                                emailVerificationPort.save(revockedCurrentEmail);
 
-                        return Mono.error(new EmailVerificationIsExpiredException());
-                    }
+                                                return Mono.error(new EmailVerificationIsExpiredException());
+                                        }
 
-                    EmailVerificationModel consumed = new EmailVerificationModel.EmailVerificationBuilder()
-                            .id(token.getId())
-                            .userId(token.getUserId())
-                            .token(token.getToken())
-                            .statusEmail(StatusEmail.CONSUMED)
-                            .consumedAt(token.getConsumedAt())
-                            .createAt(token.getCreateAt())
-                            .expiredAt(token.getExpiredAt())
-                            .build();
+                                        EmailVerificationModel consumed = new EmailVerificationModel.EmailVerificationBuilder()
+                                                        .id(token.getId())
+                                                        .userId(token.getUserId())
+                                                        .token(token.getToken())
+                                                        .statusEmail(StatusEmail.CONSUMED)
+                                                        .consumedAt(token.getConsumedAt())
+                                                        .createAt(token.getCreateAt())
+                                                        .expiredAt(token.getExpiredAt())
+                                                        .build();
 
-                    // actualizamos el usuario para que pueda iniciar session
-                    UserModel changeEnabledUser = UserModel.changeStatus(user, true);
+                                        // actualizamos el usuario para que pueda iniciar session
+                                        UserModel changeEnabledUser = UserModel.changeStatus(user, true);
 
-                    return Mono.when(emailVerificationPort.save(consumed), authRepositoryPort.save(changeEnabledUser))
-                            .thenReturn(new EmailVerificationCommandResult("email verification successfull!"));
-                });
-    }
+                                        return Mono.when(emailVerificationPort.save(consumed),
+                                                        authRepositoryPort.save(changeEnabledUser))
+                                                        .thenReturn(new EmailVerificationCommandResult(
+                                                                        "Verificacion de email exitosa!"));
+                                });
+        }
 }
