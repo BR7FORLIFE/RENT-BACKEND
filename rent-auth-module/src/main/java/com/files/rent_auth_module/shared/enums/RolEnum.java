2f90210903b91e71b6481669d7b83cebacd7f7f5
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-06-25 17:41:53 -0500

    Refactor code structure for improved readability and maintainability

--- rent-auth-module/src/main/java/com/files/rent_auth_module/shared/enums/RolEnum.java
+++ rent-auth-module/src/main/java/com/files/rent_auth_module/shared/enums/RolEnum.java
@@ -1,11 +1,10 @@
 package com.files.rent_auth_module.shared.enums;
 
 public enum RolEnum {
-    USER, ADMIN, TENANT, LESSOR, LAWYER, ADMINISTRATIVE
+    USER, ADMIN
 }
 
 // TENANT -> INQUILINO
 // LESSOR -> ARRENDADOR
 // LAWYER -> abogado que hace parte del proceso de arrendamiento
 // ADMINISTRATIVE -> personal administrativo del modelo de negocio
-
