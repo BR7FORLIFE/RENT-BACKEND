9117c34fec360016bb7672dc5827bf214dfacbb0
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-06-27 19:06:03 -0500

    feat: add public key file and configure global exception filter in main module

--- rent-financial-module/src/app.module.ts
+++ rent-financial-module/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtPassport } from './core/auth/auth-strategy.js';
+import { JwtAuthGuard } from './core/auth/auth.guard.js';
+import { PropertyRegistrationModule } from './features/property-registration/property-registration.module.js';
+import { PrismaModule } from './core/database/prisma.module.js';
 
 @Module({
-  imports: [],
+  imports: [PropertyRegistrationModule, PrismaModule],
   controllers: [],
-  providers: [],
+  providers: [JwtPassport, JwtAuthGuard],
 })
 export class AppModule {}
--- rent-financial-module/src/main.ts
+++ rent-financial-module/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { GlobalExceptionFilter } from './core/filters/exception.filter.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
