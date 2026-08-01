8ce53a5d3180a37604c418ea00ea51d2e05d47f6
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-07-31 22:44:32 -0500

    feat: refactor config and env files for improved microservice authentication and environment variable management

--- rent-financial-module/src/config/config.ts
+++ rent-financial-module/src/config/config.ts
@@ -1,3 +1,20 @@
 import { Resend } from 'resend';
+import axios from 'axios';
+import { MicroserviceAuthService } from '../features/microservice-auth/auth-microservice.service.js';
+import { RESEND_API_KEY } from './env.js';
 
-export const resendClient = new Resend(process.env.RESEND_API_KEY);
+//resend config
+export const resendClient = new Resend(RESEND_API_KEY);
+
+//axios config
+export const axiosMicroserviceClient = axios.create();
+export const axiosRentAuth = axios.create();
+
+//instanciamos la clase donde extraeremos el JWT
+const microserviceService = new MicroserviceAuthService();
+
+axiosMicroserviceClient.interceptors.request.use(async (config) => {
+  const JWT_MICROSERVICE_TOKEN = await microserviceService.getToken();
+  config.headers.Authorization = `Bearer ${JWT_MICROSERVICE_TOKEN}`;
+  return config;
+});
--- rent-financial-module/src/config/env.ts
+++ rent-financial-module/src/config/env.ts
@@ -2,7 +2,6 @@ import { readFileSync } from 'fs';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-dotenv.config({ path: 'microservice-identification/microservice-id.env' }); //cargamos las variables de entorno del microservice identification
 
 //variables de identidad para el microservicio de rent financial
 export const MICROSERVICE_CLIENT_ID = process.env.CLIENT_ID;
@@ -23,3 +22,14 @@ export const POSTGRES_URI = process.env.POSTGRES_URI;
 export const OLLAMA_HOST =
   process.env.OLLAMA_HOST ?? 'http://host.docker.internal:11434';
 export const OLLAMA_MODEL = process.env.OLLAMA_MODEL ?? 'granite3-dense:2b';
+
+// MICROSERVICE IDENTIFICATION
+export const RENT_AUTH_HOST = process.env.RENT_AUTH_HOST;
+export const RENT_FINANCIAL_CLIENT_ID = process.env.RENT_FINANCIAL_CLIENT_ID;
+export const RENT_FINANCIAL_CLIENT_SECRET =
+  process.env.RENT_FINANCIAL_CLIENT_SECRET;
+
+//Resend ENVIROMENT VARIABLES
+export const RESEND_API_KEY = process.env.RESEND_API_KEY;
+
+export const ISSUER_EMAIL = process.env.ISSUER_EMAIL;
