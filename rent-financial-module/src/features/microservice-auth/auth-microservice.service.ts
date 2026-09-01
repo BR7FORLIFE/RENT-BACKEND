872d3a8c21d3ca065da8fbc0d4a6e6d3ef83e7b9
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-09-01 15:36:12 -0500

    feat: enhance MicroserviceAuthService to manage token expiration and validation

--- rent-financial-module/src/features/microservice-auth/auth-microservice.service.ts
+++ rent-financial-module/src/features/microservice-auth/auth-microservice.service.ts
@@ -15,12 +15,9 @@ export interface MicroserviceIdentificationData {
 @Injectable()
 export class MicroserviceAuthService {
   private token: string | null; // JWT
+  private expirationTime: Date | null = null;
 
   async obtainToken() {
-    if (this.token) {
-      return this.token;
-    }
-
     if (!RENT_FINANCIAL_CLIENT_ID || !RENT_FINANCIAL_CLIENT_SECRET) {
       throw new Error(
         'Se esperaba un CLIENT_ID  o un CLIENT_SECRET pero en cambio no se recibió',
@@ -32,11 +29,14 @@ export class MicroserviceAuthService {
     };
 
     try {
-      const { data } = await axiosRentAuth.post<{ jwt: string }>(
-        `${RENT_AUTH_HOST}/rent-auth/microservice-identification`,
-        body,
-      );
+      const { data } = await axiosRentAuth.post<{
+        jwt: string;
+        expiredTimeSeconds: number;
+      }>(`${RENT_AUTH_HOST}/rent-auth/microservice-identification`, body);
+
       this.token = data.jwt;
+      this.verifyJwtExpiration(data.expiredTimeSeconds); //seteamos el expiration time
+
       return data.jwt;
     } catch (error) {
       this.token = null;
@@ -51,10 +51,32 @@ export class MicroserviceAuthService {
     }
   }
 
-  getToken() {
-    if (this.token) {
+  //este metodo nos permite darnos un margen de error ya que
+  //pretendemos verificar si esta expirado el token 5 segundos antes de su expiracion
+  //para evitar cualquier bug
+  private verifyJwtExpiration(expiredTimeSeconds: number): void {
+    const SECURE_MARGIN_SECONDS = 5;
+
+    const expirationTimestamp =
+      Date.now() + (expiredTimeSeconds - SECURE_MARGIN_SECONDS) * 1000;
+
+    this.expirationTime = new Date(expirationTimestamp); //lo seteamos en la variable
+  }
+
+  //este metodo nos dira si el token esta expirado en este caso por el tiempo de expiracion
+  //que nos da el microservicio de auth
+  private isTokenExpired(): boolean {
+    if (!this.expirationTime) {
+      return true;
+    }
+    return Date.now() >= this.expirationTime.getTime();
+  }
+
+  async getToken(): Promise<string> {
+    if (this.token && !this.isTokenExpired()) {
       return this.token;
     }
+
     return this.obtainToken();
   }
 }
